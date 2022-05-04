TomasHofman/el-ri

Added changes from https://github.com/jboss/jboss-el-api_spec

api/src/main/java/javax/el/ELUtil.java
@@ -23,6 +23,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,6 +48,7 @@ import java.util.ResourceBundle;
  * @author Dongbin Nie
  */
 class ELUtil {
+    private static final String EL_BC22_PROPERTY= "org.wildfly.el.bc2.2";
 
     /**
      * This class may not be constructed.
@@ -53,11 +56,23 @@ class ELUtil {
     private ELUtil() {
     }
 
-    /*
-     * For testing Backward Compatibility option static java.util.Properties properties = new java.util.Properties(); static
-     * { properties.setProperty("javax.el.bc2.2", "true"); }
-     */
-    public static ExpressionFactory exprFactory = ExpressionFactory.newInstance(/* properties */);
+    static final java.util.Properties properties = new java.util.Properties();
+    private static ExpressionFactory exprFactory = null;
+    static {
+        setupProperties();
+    }
+
+    private static void setupProperties(){
+        boolean bc22Enabled = AccessController.doPrivileged(new PrivilegedAction<Boolean>() {
+            public Boolean run() {
+                return Boolean.getBoolean(EL_BC22_PROPERTY);
+            }
+
+        });
+        if (bc22Enabled) {
+            properties.setProperty("javax.el.bc2.2", "true");
+        }
+    }
 
     /**
      * <p>
@@ -166,6 +181,9 @@ class ELUtil {
     }
 
     static ExpressionFactory getExpressionFactory() {
+        if (exprFactory == null){
+            exprFactory = ExpressionFactory.newInstance(properties);
+        }
         return exprFactory;
     }
 
@@ -217,6 +235,8 @@ class ELUtil {
         return method;
     }
 
+/*TODO:  Verify if Jakarta or older JBoss spec fork copy of below code is better */ 
+
     /*
      * This method duplicates code in com.sun.el.util.ReflectionUtil. When making changes keep the code in sync.
      */
@@ -248,7 +268,7 @@ class ELUtil {
 
         Method[] methods = clazz.getMethods();
 
-        List<Wrapper> wrappers = Wrapper.wrap(methods, methodName);
+        List<Wrapper> wrappers = Wrapper.wrap(clazz, methods, methodName);
 
         Wrapper result = findWrapper(clazz, wrappers, methodName, paramTypes, paramValues);
 
@@ -702,10 +722,12 @@ class ELUtil {
      */
     private abstract static class Wrapper {
 
-        public static List<Wrapper> wrap(Method[] methods, String name) {
+        public static List<Wrapper> wrap(Class<?> clazz, Method[] methods, String name) {
             List<Wrapper> result = new ArrayList<>();
             for (Method method : methods) {
-                if (method.getName().equals(name)) {
+                if (method.getName().equals(name)
+                        // bridge check added for https://issues.jboss.org/browse/JBEE-165
+                        && !(method.isBridge() && !method.getDeclaringClass().equals(clazz))) {
                     result.add(new MethodWrapper(method));
                 }
             }

api/src/main/java/javax/el/Util.java
@@ -1,0 +1,93 @@
+/*
+ * ---------------
+ *
+ * Copied from https://github.com/apache/tomcat/blob/trunk/java/javax/el/Util.java
+ *
+ * ---------------
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package javax.el;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+class Util {
+
+    static String message(ELContext context, String name, Object... props) {
+        Locale locale = null;
+        if (context != null) {
+            locale = context.getLocale();
+        }
+        if (locale == null) {
+            locale = Locale.getDefault();
+            if (locale == null) {
+                return "";
+            }
+        }
+        ResourceBundle bundle = ResourceBundle.getBundle(
+                "javax.el.PrivateMessages", locale);
+        try {
+            String template = bundle.getString(name);
+            if (props != null) {
+                template = MessageFormat.format(template, props);
+            }
+            return template;
+        } catch (MissingResourceException e) {
+            return "Missing Resource: '" + name + "' for Locale "
+                    + locale.getDisplayName();
+        }
+    }
+
+    /*
+     * This method duplicates code in org.apache.el.util.ReflectionUtil. When
+     * making changes keep the code in sync.
+     */
+    static boolean isAssignableFrom(Class<?> src, Class<?> target) {
+        // src will always be an object
+        // Short-cut. null is always assignable to an object and in EL null
+        // can always be coerced to a valid value for a primitive
+        if (src == null) {
+            return true;
+        }
+
+        Class<?> targetClass;
+        if (target.isPrimitive()) {
+            if (target == Boolean.TYPE) {
+                targetClass = Boolean.class;
+            } else if (target == Character.TYPE) {
+                targetClass = Character.class;
+            } else if (target == Byte.TYPE) {
+                targetClass = Byte.class;
+            } else if (target == Short.TYPE) {
+                targetClass = Short.class;
+            } else if (target == Integer.TYPE) {
+                targetClass = Integer.class;
+            } else if (target == Long.TYPE) {
+                targetClass = Long.class;
+            } else if (target == Float.TYPE) {
+                targetClass = Float.class;
+            } else {
+                targetClass = Double.class;
+            }
+        } else {
+            targetClass = target;
+        }
+        return targetClass.isAssignableFrom(src);
+    }
+
+}

api/src/main/java/org/jboss/el/cache/BeanPropertiesCache.java
@@ -1,0 +1,207 @@
+package org.jboss.el.cache;
+
+import javax.el.ELException;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Stuart Douglas
+ */
+public class BeanPropertiesCache {
+
+    static private class BPSoftReference extends SoftReference<BeanProperties> {
+        final Class<?> key;
+        BPSoftReference(Class<?> key, BeanProperties beanProperties,
+                        ReferenceQueue<BeanProperties> refQ) {
+            super(beanProperties, refQ);
+            this.key = key;
+        }
+    }
+
+    public static class SoftConcurrentHashMap extends
+            ConcurrentHashMap<Class<?>, BeanProperties> {
+
+        private static final int CACHE_INIT_SIZE = 1024;
+        private ConcurrentHashMap<Class<?>, BPSoftReference> map =
+                new ConcurrentHashMap<Class<?>, BPSoftReference>(CACHE_INIT_SIZE);
+        private ReferenceQueue<BeanProperties> refQ =
+                new ReferenceQueue<BeanProperties>();
+
+        // Remove map entries that have been placed on the queue by GC.
+        private void cleanup() {
+            BPSoftReference BPRef = null;
+            while ((BPRef = (BPSoftReference)refQ.poll()) != null) {
+                map.remove(BPRef.key);
+            }
+        }
+
+        protected void clear(ClassLoader classLoader) {
+            Iterator<Map.Entry<Class<?>, BPSoftReference>> it = map.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry<Class<?>, BPSoftReference> entry = it.next();
+                if(entry.getKey().getClassLoader() == classLoader) {
+                    it.remove();
+                }
+            }
+
+        }
+
+        @Override
+        public BeanProperties put(Class<?> key, BeanProperties value) {
+            cleanup();
+            BPSoftReference prev =
+                    map.put(key, new BPSoftReference(key, value, refQ));
+            return prev == null? null: prev.get();
+        }
+
+        @Override
+        public BeanProperties putIfAbsent(Class<?> key, BeanProperties value) {
+            cleanup();
+            BPSoftReference prev =
+                    map.putIfAbsent(key, new BPSoftReference(key, value, refQ));
+            return prev == null? null: prev.get();
+        }
+
+        @Override
+        public BeanProperties get(Object key) {
+            cleanup();
+            BPSoftReference BPRef = map.get(key);
+            if (BPRef == null) {
+                return null;
+            }
+            if (BPRef.get() == null) {
+                // value has been garbage collected, remove entry in map
+                map.remove(key);
+                return null;
+            }
+            return BPRef.get();
+        }
+    }
+    /*
+     * Defines a property for a bean.
+     */
+    public final static class BeanProperty {
+
+        private Method readMethod;
+        private Method writeMethod;
+        private PropertyDescriptor descriptor;
+
+        public BeanProperty(Class<?> baseClass,
+                            PropertyDescriptor descriptor) {
+            this.descriptor = descriptor;
+            readMethod = getMethod(baseClass, descriptor.getReadMethod());
+            writeMethod = getMethod(baseClass, descriptor.getWriteMethod());
+        }
+
+        public Class getPropertyType() {
+            return descriptor.getPropertyType();
+        }
+
+        public boolean isReadOnly() {
+            return getWriteMethod() == null;
+        }
+
+        public Method getReadMethod() {
+            return readMethod;
+        }
+
+        public Method getWriteMethod() {
+            return writeMethod;
+        }
+    }
+
+    /*
+     * Defines the properties for a bean.
+     */
+    public final static class BeanProperties {
+
+        private final Map<String, BeanProperty> propertyMap =
+                new HashMap<String, BeanProperty>();
+
+        public BeanProperties(Class<?> baseClass) {
+            PropertyDescriptor[] descriptors;
+            try {
+                BeanInfo info = Introspector.getBeanInfo(baseClass);
+                descriptors = info.getPropertyDescriptors();
+            } catch (IntrospectionException ie) {
+                throw new ELException(ie);
+            }
+            for (PropertyDescriptor pd: descriptors) {
+                propertyMap.put(pd.getName(),
+                        new BeanProperty(baseClass, pd));
+            }
+        }
+
+        public BeanProperty getBeanProperty(String property) {
+            return propertyMap.get(property);
+        }
+    }
+
+    /**
+     * sfot references are horrible
+     */
+    private static final SoftConcurrentHashMap properties =
+            new SoftConcurrentHashMap();
+
+
+    /*
+     * Get a public method form a public class or interface of a given method.
+     * Note that if a PropertyDescriptor is obtained for a non-public class that
+     * implements a public interface, the read/write methods will be for the
+     * class, and therefore inaccessible.  To correct this, a version of the
+     * same method must be found in a superclass or interface.
+     **/
+
+    public static Method getMethod(Class<?> cl, Method method) {
+
+        if (method == null) {
+            return null;
+        }
+
+        if (Modifier.isPublic(cl.getModifiers())) {
+            return method;
+        }
+        Class<?> [] interfaces = cl.getInterfaces ();
+        for (int i = 0; i < interfaces.length; i++) {
+            Class<?> c = interfaces[i];
+            Method m = null;
+            try {
+                m = c.getMethod(method.getName(), method.getParameterTypes());
+                c = m.getDeclaringClass();
+                if ((m = getMethod(c, m)) != null)
+                    return m;
+            } catch (NoSuchMethodException ex) {
+            }
+        }
+        Class<?> c = cl.getSuperclass();
+        if (c != null) {
+            Method m = null;
+            try {
+                m = c.getMethod(method.getName(), method.getParameterTypes());
+                c = m.getDeclaringClass();
+                if ((m = getMethod(c, m)) != null)
+                    return m;
+            } catch (NoSuchMethodException ex) {
+            }
+        }
+        return null;
+    }
+
+    public static SoftConcurrentHashMap getProperties() {
+        return properties;
+    }
+
+    static void clear(ClassLoader classLoader) {
+        properties.clear(classLoader);
+    }
+}

scottmarlow/jboss-jakarta-el-api_spec

api/src/main/java/jakarta/el/ELUtil.java
@@ -23,6 +23,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,6 +48,7 @@ import java.util.ResourceBundle;
  * @author Dongbin Nie
  */
 class ELUtil {
+    private static final String EL_BC22_PROPERTY= "org.wildfly.el.bc2.2";
 
     /**
      * This class may not be constructed.
@@ -53,11 +56,23 @@ class ELUtil {
     private ELUtil() {
     }
 
-    /*
-     * For testing Backward Compatibility option static java.util.Properties properties = new java.util.Properties(); static
-     * { properties.setProperty("jakarta.el.bc2.2", "true"); }
-     */
-    public static ExpressionFactory exprFactory = ExpressionFactory.newInstance(/* properties */);
+    static final java.util.Properties properties = new java.util.Properties();
+    private static ExpressionFactory exprFactory = null;
+    static {
+        setupProperties();
+    }
+
+    private static void setupProperties(){
+        boolean bc22Enabled = AccessController.doPrivileged(new PrivilegedAction<Boolean>() {
+            public Boolean run() {
+                return Boolean.getBoolean(EL_BC22_PROPERTY);
+            }
+
+        });
+        if (bc22Enabled) {
+            properties.setProperty("jakarta.el.bc2.2", "true");
+        }
+    }
 
     /**
      * <p>
@@ -166,6 +181,9 @@ class ELUtil {
     }
 
     static ExpressionFactory getExpressionFactory() {
+        if (exprFactory == null){
+            exprFactory = ExpressionFactory.newInstance(properties);
+        }
         return exprFactory;
     }
 
@@ -248,7 +266,7 @@ class ELUtil {
 
         Method[] methods = clazz.getMethods();
 
-        List<Wrapper> wrappers = Wrapper.wrap(methods, methodName);
+        List<Wrapper> wrappers = Wrapper.wrap(clazz, methods, methodName);
 
         Wrapper result = findWrapper(clazz, wrappers, methodName, paramTypes, paramValues);
 
@@ -699,10 +717,12 @@ class ELUtil {
      */
     private abstract static class Wrapper {
 
-        public static List<Wrapper> wrap(Method[] methods, String name) {
+        public static List<Wrapper> wrap(Class<?> clazz, Method[] methods, String name) {
             List<Wrapper> result = new ArrayList<>();
             for (Method method : methods) {
-                if (method.getName().equals(name)) {
+                if (method.getName().equals(name)
+                        // bridge check added for https://issues.jboss.org/browse/JBEE-165
+                        && !(method.isBridge() && !method.getDeclaringClass().equals(clazz))) {
                     result.add(new MethodWrapper(method));
                 }
             }

api/src/main/java/jakarta/el/Util.java
@@ -1,0 +1,93 @@
+/*
+ * ---------------
+ *
+ * Copied from https://github.com/apache/tomcat/blob/trunk/java/javax/el/Util.java
+ *
+ * ---------------
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jakarta.el;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+class Util {
+
+    static String message(ELContext context, String name, Object... props) {
+        Locale locale = null;
+        if (context != null) {
+            locale = context.getLocale();
+        }
+        if (locale == null) {
+            locale = Locale.getDefault();
+            if (locale == null) {
+                return "";
+            }
+        }
+        ResourceBundle bundle = ResourceBundle.getBundle(
+                "jakarta.el.PrivateMessages", locale);
+        try {
+            String template = bundle.getString(name);
+            if (props != null) {
+                template = MessageFormat.format(template, props);
+            }
+            return template;
+        } catch (MissingResourceException e) {
+            return "Missing Resource: '" + name + "' for Locale "
+                    + locale.getDisplayName();
+        }
+    }
+
+    /*
+     * This method duplicates code in org.apache.el.util.ReflectionUtil. When
+     * making changes keep the code in sync.
+     */
+    static boolean isAssignableFrom(Class<?> src, Class<?> target) {
+        // src will always be an object
+        // Short-cut. null is always assignable to an object and in EL null
+        // can always be coerced to a valid value for a primitive
+        if (src == null) {
+            return true;
+        }
+
+        Class<?> targetClass;
+        if (target.isPrimitive()) {
+            if (target == Boolean.TYPE) {
+                targetClass = Boolean.class;
+            } else if (target == Character.TYPE) {
+                targetClass = Character.class;
+            } else if (target == Byte.TYPE) {
+                targetClass = Byte.class;
+            } else if (target == Short.TYPE) {
+                targetClass = Short.class;
+            } else if (target == Integer.TYPE) {
+                targetClass = Integer.class;
+            } else if (target == Long.TYPE) {
+                targetClass = Long.class;
+            } else if (target == Float.TYPE) {
+                targetClass = Float.class;
+            } else {
+                targetClass = Double.class;
+            }
+        } else {
+            targetClass = target;
+        }
+        return targetClass.isAssignableFrom(src);
+    }
+
+}

api/src/main/java/org/jboss/el/cache/BeanPropertiesCache.java
@@ -1,0 +1,207 @@
+package org.jboss.el.cache;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import jakarta.el.ELException;
+
+/**
+ * @author Stuart Douglas
+ */
+public class BeanPropertiesCache {
+
+    static private class BPSoftReference extends SoftReference<BeanProperties> {
+        final Class<?> key;
+        BPSoftReference(Class<?> key, BeanProperties beanProperties,
+                        ReferenceQueue<BeanProperties> refQ) {
+            super(beanProperties, refQ);
+            this.key = key;
+        }
+    }
+
+    public static class SoftConcurrentHashMap extends
+            ConcurrentHashMap<Class<?>, BeanProperties> {
+
+        private static final int CACHE_INIT_SIZE = 1024;
+        private ConcurrentHashMap<Class<?>, BPSoftReference> map =
+                new ConcurrentHashMap<Class<?>, BPSoftReference>(CACHE_INIT_SIZE);
+        private ReferenceQueue<BeanProperties> refQ =
+                new ReferenceQueue<BeanProperties>();
+
+        // Remove map entries that have been placed on the queue by GC.
+        private void cleanup() {
+            BPSoftReference BPRef = null;
+            while ((BPRef = (BPSoftReference)refQ.poll()) != null) {
+                map.remove(BPRef.key);
+            }
+        }
+
+        protected void clear(ClassLoader classLoader) {
+            Iterator<Map.Entry<Class<?>, BPSoftReference>> it = map.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry<Class<?>, BPSoftReference> entry = it.next();
+                if(entry.getKey().getClassLoader() == classLoader) {
+                    it.remove();
+                }
+            }
+
+        }
+
+        @Override
+        public BeanProperties put(Class<?> key, BeanProperties value) {
+            cleanup();
+            BPSoftReference prev =
+                    map.put(key, new BPSoftReference(key, value, refQ));
+            return prev == null? null: prev.get();
+        }
+
+        @Override
+        public BeanProperties putIfAbsent(Class<?> key, BeanProperties value) {
+            cleanup();
+            BPSoftReference prev =
+                    map.putIfAbsent(key, new BPSoftReference(key, value, refQ));
+            return prev == null? null: prev.get();
+        }
+
+        @Override
+        public BeanProperties get(Object key) {
+            cleanup();
+            BPSoftReference BPRef = map.get(key);
+            if (BPRef == null) {
+                return null;
+            }
+            if (BPRef.get() == null) {
+                // value has been garbage collected, remove entry in map
+                map.remove(key);
+                return null;
+            }
+            return BPRef.get();
+        }
+    }
+    /*
+     * Defines a property for a bean.
+     */
+    public final static class BeanProperty {
+
+        private Method readMethod;
+        private Method writeMethod;
+        private PropertyDescriptor descriptor;
+
+        public BeanProperty(Class<?> baseClass,
+                            PropertyDescriptor descriptor) {
+            this.descriptor = descriptor;
+            readMethod = getMethod(baseClass, descriptor.getReadMethod());
+            writeMethod = getMethod(baseClass, descriptor.getWriteMethod());
+        }
+
+        public Class getPropertyType() {
+            return descriptor.getPropertyType();
+        }
+
+        public boolean isReadOnly() {
+            return getWriteMethod() == null;
+        }
+
+        public Method getReadMethod() {
+            return readMethod;
+        }
+
+        public Method getWriteMethod() {
+            return writeMethod;
+        }
+    }
+
+    /*
+     * Defines the properties for a bean.
+     */
+    public final static class BeanProperties {
+
+        private final Map<String, BeanProperty> propertyMap =
+                new HashMap<String, BeanProperty>();
+
+        public BeanProperties(Class<?> baseClass) {
+            PropertyDescriptor[] descriptors;
+            try {
+                BeanInfo info = Introspector.getBeanInfo(baseClass);
+                descriptors = info.getPropertyDescriptors();
+            } catch (IntrospectionException ie) {
+                throw new ELException(ie);
+            }
+            for (PropertyDescriptor pd: descriptors) {
+                propertyMap.put(pd.getName(),
+                        new BeanProperty(baseClass, pd));
+            }
+        }
+
+        public BeanProperty getBeanProperty(String property) {
+            return propertyMap.get(property);
+        }
+    }
+
+    /**
+     * sfot references are horrible
+     */
+    private static final SoftConcurrentHashMap properties =
+            new SoftConcurrentHashMap();
+
+
+    /*
+     * Get a public method form a public class or interface of a given method.
+     * Note that if a PropertyDescriptor is obtained for a non-public class that
+     * implements a public interface, the read/write methods will be for the
+     * class, and therefore inaccessible.  To correct this, a version of the
+     * same method must be found in a superclass or interface.
+     **/
+
+    public static Method getMethod(Class<?> cl, Method method) {
+
+        if (method == null) {
+            return null;
+        }
+
+        if (Modifier.isPublic(cl.getModifiers())) {
+            return method;
+        }
+        Class<?> [] interfaces = cl.getInterfaces ();
+        for (int i = 0; i < interfaces.length; i++) {
+            Class<?> c = interfaces[i];
+            Method m = null;
+            try {
+                m = c.getMethod(method.getName(), method.getParameterTypes());
+                c = m.getDeclaringClass();
+                if ((m = getMethod(c, m)) != null)
+                    return m;
+            } catch (NoSuchMethodException ex) {
+            }
+        }
+        Class<?> c = cl.getSuperclass();
+        if (c != null) {
+            Method m = null;
+            try {
+                m = c.getMethod(method.getName(), method.getParameterTypes());
+                c = m.getDeclaringClass();
+                if ((m = getMethod(c, m)) != null)
+                    return m;
+            } catch (NoSuchMethodException ex) {
+            }
+        }
+        return null;
+    }
+
+    public static SoftConcurrentHashMap getProperties() {
+        return properties;
+    }
+
+    static void clear(ClassLoader classLoader) {
+        properties.clear(classLoader);
+    }
+}